99580e5762b57e76f434597353e0d11f74c1566a
alexalexyang <alexalexyang@gmail.com>
2022-06-19 15:47:45 +0200

    Landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/tailwind-preflight.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+// export default process.env.NODE_ENV === "development"
+//   ? dynamic(() => Promise.resolve(App), {
+//       ssr: false,
+//     })
+//   : App;
+
+export default App;
---
+++ pages/photos.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Photos: NextPage = () => {
+  return <h1>Photos</h1>;
+};
+
+export default Photos;
